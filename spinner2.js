f4f15bcfbca5fdbee0e89f64aa8a402b0a69770c
Hind <hind.nayhi@gmail.com>
2022-08-02 13:02:59 -0600

    Refactored setTimeout.

--- spinner2.js
+++ spinner2.js
@@ -1,15 +1,15 @@
 //Refactor the code from spinner1.js and make DRY.
 const spinner = '|/-\\|/-\\|';
-let i = 0;
+let count = 0;
 
 const animate = function() {
   setTimeout(() => {
-    process.stdout.write('\r' + spinner[i++]); //Increment each time by 1.
+    process.stdout.write('\r' + spinner[count++]); //Increment each time by 1.
 
-    if (i < spinner.length) { //Keep calling animate() as long as the condition is true.
+    if (count < spinner.length) { //Keep calling animate() as long as the condition is true.
       animate();
     }
-    if (i === spinner.length) { //When it reaches the last element, go back to a new line.
+    if (count === spinner.length) { //When it reaches the last element, go back to a new line.
       process.stdout.write('\n');
     }
   }, 100);
